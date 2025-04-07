bce43376aa8f95890f95d9bccb29c731ee47b2f2
Mazharul <mazharul.jatri@gmail.com>
2025-04-07 18:04:29 +0600

    javascript engine

---
+++ ztm/theory/javascript-engine.js
@@ -0,0 +1,18 @@
+// Javascript Engine
+// JS Engine is the core of JavaScript to execute javascript code in Computer. This understands the js code and convert it into machine-readable code
+// There are lots of JS engine developed by different org. One of the most popular JavaScript engines is v8 Engine which is developed by Google written in c++
+
+
+// What's Inside JavaScript engine or Step to Js file to machine-readable code
+// 1> hello.js
+// 2> Parse (Lexical Analysis) --- Breaks the code
+// 3> AST ( Abstract Syntax Tree )
+// 4> Interpreter
+// 5> Profiler
+// 6> Compiler
+// 7> Optimized Code
+// 8> Binary Code
+
+// Another more important part of engine is memory management
+// 1> Call Stack
+// 2> Memory Heap
