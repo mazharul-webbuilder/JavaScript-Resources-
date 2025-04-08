4814b47b20809484eab2e133ce41983ed7936f8c
Mazharul <mazharul.jatri@gmail.com>
2025-04-08 15:12:55 +0600

    javascript engine details with JIT compiler explain

--- ztm/theory/javascript-engine.js
+++ ztm/theory/javascript-engine.js
@@ -7,12 +7,15 @@
 // 1> hello.js
 // 2> Parse (Lexical Analysis) --- Breaks the code
 // 3> AST ( Abstract Syntax Tree )
-// 4> Interpreter
-// 5> Profiler
-// 6> Compiler
+// 4> Interpreter (Covert to byte code)
+// 5> Profiler (Looking for optimize the code)
+// 6> Compiler (JIT compiler) - ex: in google its called "turbofan"
 // 7> Optimized Code
 // 8> Binary Code
 
 // Another more important part of engine is memory management
 // 1> Call Stack
 // 2> Memory Heap
+
+// JIT COMPILER
+// Just in Time Compiler is a compiler that compiles code on the fly as it runs combined with (interpreter and compiler)
